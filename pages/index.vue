94c09c7a64c060bd027410cc319d5fa083736163
zhuyc <zyc199777@outlook.com>
2020-07-28 18:02:26 +0800

    添加登录注册页面

--- pages/index.vue
+++ pages/index.vue
@@ -4,19 +4,27 @@
     <!-- 幻灯片 开始 -->
   <div v-swiper:mySwiper="swiperOption">
       <div class="swiper-wrapper">
-
           <div v-for="banner in bannerList" :key="banner.id" class="swiper-slide" style="background: #040B1B;">
               <a target="_blank" :href="banner.linkUrl">
                   <img :src="banner.imageUrl" :alt="banner.title">
               </a>
           </div>
       </div>
-      <div class="swiper-pagination swiper-pagination-white"></div>
+      
+      <!-- <div class="swiper-pagination swiper-pagination-white"></div>
       <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
-      <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+      <div class="swiper-button-next swiper-button-white" slot="button-next"></div> -->
+      <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+    
+    <!-- 如果需要导航按钮 -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    
+    <!-- 如果需要滚动条 -->
+    <div class="swiper-scrollbar"></div>
   </div>
   <!-- 幻灯片 结束 -->
-    
      <div id="aCoursesList">
       <!-- 网校课程 开始 -->
       <div>
--- plugins/nuxt-swiper-plugin.js
+++ plugins/nuxt-swiper-plugin.js
@@ -1,4 +1,6 @@
 import Vue from "vue"
 import VueAwesomeSwiper from "vue-awesome-swiper"
+import VueQriously from 'vue-qriously'
 
 Vue.use(VueAwesomeSwiper)
+Vue.use(VueQriously)
